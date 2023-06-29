6c1d14e2ffd913c198651604908ee7384ada7534
ehuntober <ehuntercrypto@gmail.com>
2023-06-29 13:05:22 +0000

    product modified

--- controllers/products.js
+++ controllers/products.js
@@ -14,6 +14,14 @@ const getAllProducts = async (req,res) =>{
     if(featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
+
+    if (commpany){
+        queryObject.commpany = commpany
+    }
+
+    if (name) {
+        queryObject.name = name
+    }
     console.log(queryObject)
     const products = await Product.find(queryObject)
     res.status(200).json({products, nbHits: products.length})
