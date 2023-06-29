8e368704fdcadd0322ed1a3a0bac953e4b492110
ehuntober <ehuntercrypto@gmail.com>
2023-06-29 14:11:25 +0000

    product modified

--- controllers/products.js
+++ controllers/products.js
@@ -2,25 +2,28 @@
 const Product = require('../models/products')
 
 const getAllProductsStatic = async (req,res) =>{
-    const products = await Product.find({})
+    const search = 'a'
+    const products = await Product.find({
+        name: {$regex: search, $options: 'i'},
+    })
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) =>{
 
-    const {featured, name}  = req.query
+    const {featured, name, company}  = req.query
     const queryObject = {}
 
     if(featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
 
-    // if (company){
-    //     queryObject.company = company
-    // }
+    if (company){
+        queryObject.company = company
+    }
 
     if (name) {
-        queryObject.name = name
+        queryObject.name ={$regex: name, $options: 'i'}
     }
     console.log(queryObject)
     const products = await Product.find(queryObject)
