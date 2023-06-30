d286e9c69ab2b1a9367087e54b6f923fc2a2a47d
ehuntober <ehuntercrypto@gmail.com>
2023-06-30 11:17:35 +0000

    products modified

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,13 @@
 const Product = require('../models/products')
 
 const getAllProductsStatic = async (req,res) =>{
-    const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({ price: {$gt: 30}}).sort('name').select('name price').limit(2).skip(2)
     res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) =>{
 
-    const {featured, name, company, sort, fields}  = req.query
+    const {featured, name, company, sort, fields, numericFilters}  = req.query
     const queryObject = {}
 
     if(featured) {
@@ -21,6 +21,22 @@ const getAllProducts = async (req,res) =>{
 
     if (name) {
         queryObject.name ={$regex: name, $options: 'i'}
+    }
+
+
+    if (numericFilters){
+
+        const operatorMap = {
+            '>' : '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+
+        }
+        const reqEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numbericFilters.replace(RegExp, (match)=> `-${operatorMap[match]}-`)
+        console.log(numericFilters)
     }
     // console.log(queryObject)
     let result = Product.find(query)
@@ -36,8 +52,16 @@ const getAllProducts = async (req,res) =>{
         const fieldsList = fields.split(',').join('')
         result = result.select(fieldsList)
     }
+const page = Number(req.query.page) || 1
+const limit = Number(req.query.limit) || 10
+const skip = (page -1) * limit;
 
-    const products = await Product.find(queryObject)
+
+result = result.skip(skip).limit(limit)
+
+const products = await result
+
+    // const products = await Product.find(queryObject)
  
     res.status(200).json({products, nbHits: products.length})
 }
